10eaa9dd346fc643f245fc5b666b5d2854220075
kp.lu <lukangping@gmail.com>
2026-03-13 01:38:23 +0800

    Update vpaid-interactive-engine.js

--- vpaid-interactive-engine.js
+++ vpaid-interactive-engine.js
@@ -1,18 +1,45 @@
-// --- 必须补齐的方法存根，防止 SDK 报错 ---
+/**
+ * 补全所有 IAB 规定的必选方法，消除 901 错误
+ */
 
-// 修复你遇到的 getAdIcons 错误
-VpaidAd.prototype.getAdIcons = function() {
-    return false; // 返回 false 表示没有自定义图标
-};
-
-// 建议一并补齐以下方法，防止后续报其它的 missing function
-VpaidAd.prototype.getAdDuration = function() { return this._config.duration || 15; };
-VpaidAd.prototype.getAdRemainingTime = function() { return -2; }; // 协议规定：不支持返回 -2
-VpaidAd.prototype.getAdExpanded = function() { return false; };
-VpaidAd.prototype.getAdSkippableState = function() { return true; };
+// --- 属性读取方法 (Getters) ---
+VpaidAd.prototype.getAdLinear = function() { return true; };
 VpaidAd.prototype.getAdWidth = function() { return window.innerWidth; };
 VpaidAd.prototype.getAdHeight = function() { return window.innerHeight; };
+VpaidAd.prototype.getAdExpanded = function() { return false; };
+VpaidAd.prototype.getAdSkippableState = function() { return true; };
+VpaidAd.prototype.getAdRemainingTime = function() { return -2; }; // -2 表示不支持
+VpaidAd.prototype.getAdDuration = function() { return this._config ? this._config.duration : 15; };
 VpaidAd.prototype.getAdVolume = function() { return 1.0; };
+VpaidAd.prototype.getAdCompanions = function() { return ''; };
+VpaidAd.prototype.getAdIcons = function() { return false; }; // 修复你遇到的具体错误
 
-// 确保以下 setter 也有定义
-VpaidAd.prototype.setAdVolume = function(val) {};
+// --- 属性设置方法 (Setters) ---
+VpaidAd.prototype.setAdVolume = function(val) { console.log("VPAID: Set Volume", val); };
+
+// --- 广告生命周期控制方法 ---
+VpaidAd.prototype.resizeAd = function(width, height, viewMode) {
+    console.log("VPAID: Resize to", width, height);
+};
+VpaidAd.prototype.pauseAd = function() {
+    if (this._videoSlot) this._videoSlot.pause();
+    this.callEvent('AdPaused');
+};
+VpaidAd.prototype.resumeAd = function() {
+    if (this._videoSlot) this._videoSlot.play();
+    this.callEvent('AdPlaying');
+};
+VpaidAd.prototype.expandAd = function() { };
+VpaidAd.prototype.collapseAd = function() { };
+VpaidAd.prototype.skipAd = function() {
+    this.callEvent('AdSkipped');
+    this.stopAd();
+};
+
+// --- 事件监听标准接口 ---
+VpaidAd.prototype.subscribe = function(callback, eventName, context) {
+    this._eventsCallbacks[eventName] = callback.bind(context);
+};
+VpaidAd.prototype.unsubscribe = function(eventName) {
+    delete this._eventsCallbacks[eventName];
+};
